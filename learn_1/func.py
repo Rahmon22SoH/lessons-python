215c2a5b094b3cc64ffb25cd8906651ad15c660d
Rahmon22SoH <91434562+Rahmon22SoH@users.noreply.github.com>
2022-05-28 20:59:25 +0300

    file ignore

--- learn_1/func.py
+++ learn_1/func.py
@@ -1,11 +1,11 @@
 # Функция 
 # вызов из разных мест программы
 # функции помогают структурировать код и разбить его на отдельные действия 
-# делают код более читабельным и упращают поиск ошибок
+# делают код более читабельным и упрощают поиск ошибок
 # должна хорошо отражать свое действие 
-# Аргументы 
-# функуия  это чёрный яшик в который мы можем что-то положить, на выходе получить результат.
-# Аргумены - данные с которыми мы будем работать в нутри функции
+# Аргументы (могут быть позиционные и именованные)
+# функция  это чёрный ящик в который мы можем что-то положить, на выходе получить результат.
+# Аргументы - данные с которыми мы будем работать в нутрии функции
 # Названия аргументов как у обычных переменных 
 # 
 
@@ -20,15 +20,15 @@ def descounting(price, discount):
     return pirce_wich_dis
     
 
-product = {
+product = [
     {'name': 'samsung','stock': 24,'price': 1024.1, 'discount': 20,},
     {'name': 'lenovo','stock': 24,'price': 10224.1,'discount': 20},
     {'name': 'hp','stock': 24,'price': 1124.1,'discount': 20}
-}
+]
     
  #product['with_discound'] = descounting(product[0]['price'][2])
-product = descounting(product['price'],product['discount'])
-product['discount price'] = descounting(product['price'], product['discount'])
-print(type(product))
+product = descounting(product[0]['price'],product[0]['discount']) 
+print(product)
 
-
+product = descounting(product[0,1,2]['price'],product[0,1,2]['discount']) # Как рассчитать все колони ( ПРОБЛЕМА с доступом к внутренним элементам словаря и их взаимодействие с функцией)
+print(product)
--- learn_1/test.py
+++ learn_1/test.py
@@ -6,7 +6,13 @@ stok = [
     {'name': 'lenovo','stock': 24,'price': 10224.1,},
     {'name': 'hp','stock': 24,'price': 1124.1,}
 ]
-
-print(stok['price'][0:2])
+pprint(stok[0]['recomend'][0])
+stok[0]['recomend'].append('asus')
+print(stok[0]['recomend'])
+stok[0]['price'] = stok[0]['price'] - 500
+print(stok[0]['price'])
+stok[1]['name'] = stok[1] = 'asus'
+print(stok[1])
+print(stok[2]['price'])
 
 
